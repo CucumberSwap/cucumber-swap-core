f3a52b328a7e682283145ca2c1d9d92cf79f0966
chef <0xcryptocucumber@gmail.com>
2022-10-30 22:33:23 +0800

    update : path
    update import path

--- contracts/CucumberPair.sol
+++ contracts/CucumberPair.sol
@@ -170,7 +170,7 @@ contract CucumberPair is ICucumberPair, CucumberERC20 {
             require(to != _token0 && to != _token1, 'Cucumber: INVALID_TO');
             if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
             if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-            if (data.length > 0) ICucumberCallee(to).CucumberCall(msg.sender, amount0Out, amount1Out, data);
+            if (data.length > 0) ICucumberCallee(to).cucumberCall(msg.sender, amount0Out, amount1Out, data);
             balance0 = IERC20(_token0).balanceOf(address(this));
             balance1 = IERC20(_token1).balanceOf(address(this));
         }
